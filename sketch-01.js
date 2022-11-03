94c0beeb3dad16184e4bb64b1f1fc7678b84ce42
yesthis1please <yesthis1please@gmail.com>
2022-11-03 17:44:42 +0900

    started sketch-02

--- sketch-01.js
+++ sketch-01.js
@@ -6,9 +6,10 @@ const settings = {
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = "white";
+    context.fillStyle = "black";
     context.fillRect(0, 0, width, height);
     context.lineWidth = width * 0.01;
+    context.strokeStyle = "white";
 
     const w = width * 0.10;
     const h = height * 0.10;
