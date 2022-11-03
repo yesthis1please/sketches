c75a0ab4f0f412a752f71b20f28e8b52322af15d
yesthis1please <yesthis1please@gmail.com>
2022-11-03 18:03:23 +0900

    installed utility functions

--- sketch-02.js
+++ sketch-02.js
@@ -1,12 +1,18 @@
 const canvasSketch = require("canvas-sketch");
+const math = require('canvas-sketch-util/math');
+const random = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [1080, 1080],
 };
 
-const degToRad = (degrees) => {
-  return (degrees / 180) * Math.PI;
-};
+// const degToRad = (degrees) => {
+//   return (degrees / 180) * Math.PI;
+// };
+
+// const randomRange = (min, max) => {
+// return Math.random() * (max - min) + min;
+// };
 
 const sketch = () => {
   return ({ context, width, height }) => {
@@ -26,7 +32,7 @@ const sketch = () => {
     const radius = width * 0.3;
 
     for (let i = 0; i < num; i++) {
-      const slice = degToRad(360 / num);
+      const slice = math.degToRad(360 / num);
       const angle = slice * i;
 
       x = cx + radius * Math.sin(angle);
@@ -35,6 +41,7 @@ const sketch = () => {
       context.save();
       context.translate(x, y);
       context.rotate(-angle);
+      context.scale(random.range(1, 3), 1);
 
       context.beginPath();
       context.rect(-w * 0.5, -h * 0.5, w, h);
